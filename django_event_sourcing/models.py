89d7ddb1954caea617578428e2bf4d5c9fbccaf1
Vikash Tank <vikashtank@hotmail.co.uk>
2020-12-30 14:31:29 +0000

    Create event model

--- django_event_sourcing/models.py
+++ django_event_sourcing/models.py
@@ -1,5 +1,7 @@
 import enum
+import uuid
 
+from django.conf import settings
 from django.db import models
 
 
@@ -10,3 +12,12 @@ class EventType(str, enum.Enum):
     def fully_qualified_value(self):
         return '.'.join((self.get_namespace(),  self.value))
 
+
+class Event(models.Model):
+    """Represents an action that will happen."""
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    raw_type = models.CharField(max_length=255, db_index=True)
+    payload = models.JSONField()
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name="events")
